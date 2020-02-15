e9a25ed2776fdbd2a8e2a680cce7470af1213a09
Matt Ranger <matthieu.ranger@outlook.com>
2020-02-15 10:42:26 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="graph2vec-learn",
-    version="0.1.1",
+    version="0.1.2",
     license='MIT',
     description='Fast networkx graph node embeddings',
     author='Matt Ranger',
