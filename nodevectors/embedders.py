0693a89e0ab0801199dec764255dec60d36f7d24
Matt Ranger <matthieu.ranger@outlook.com>
2020-03-06 17:30:12 -0500

    Update embedders.py

--- nodevectors/embedders.py
+++ nodevectors/embedders.py
@@ -35,6 +35,14 @@ class BaseNodeEmbedder(BaseEstimator):
     def save(self, filename: str):
         """
         Saves model to a custom file format
+        
+        filename : str
+            Name of file to save. Don't include filename extensions
+            Extensions are added automatically
+        
+        File format is a zipfile with joblib dump (pickle-like) + dependency metata
+        Metadata is checked on load.
+        
         Includes validation and metadata to avoid Pickle deserialization gotchas
         See here Alex Gaynor PyCon 2014 talk "Pickles are for Delis"
             for more info on why we introduce this additional check
@@ -58,7 +66,14 @@ class BaseNodeEmbedder(BaseEstimator):
     @staticmethod
     def load(filename: str):
         """
-        Load model from zip file.
+        Load model from NodeEmbedding model zip file.
+        
+        filename : str
+            full filename of file to load (including extensions)
+            The file should be the result of a `save()` call
+            
+        Loading checks for metadata and raises warnings if pkg versions
+        are different than they were when saving the model.
         """
         with tempfile.TemporaryDirectory() as temp_dir:
             shutil.unpack_archive(filename, temp_dir, 'zip')
@@ -94,6 +109,14 @@ class BaseNodeEmbedder(BaseEstimator):
         Predicts a new node from its neighbors
         Generally done through the average of the embeddings
             of its neighboring nodes.
+            
+        neighbor_list : iterable[node_id or node name]
+            list of nodes with edges to the new node to be predicted
+        
+        fn : function array -> scalar
+            function to reduce the embeddings. Normally np.mean or np.sum
+            This takes the embeddings of the neighbors and reduces them 
+               to the final estimate for the unseen node
         """
         # TODO: test
         pass
@@ -101,9 +124,7 @@ class BaseNodeEmbedder(BaseEstimator):
 class Node2Vec(BaseNodeEmbedder):
     """
     Embeds NetworkX into a continuous representation of the nodes.
-
     The resulting embedding can be queried just like word embeddings.
-
     Note: the graph's node names need to be int or str.
     """
     def __init__(
@@ -126,9 +147,14 @@ class Node2Vec(BaseNodeEmbedder):
             number of times to start a walk from each nodes
         threads : int
             number of threads to use. 0 is full use
+        n_components : int
+            number of resulting dimensions for the embedding
+            This should be set here rather than in the w2vparams arguments
+        keep_walks : bool
+            Whether to save the random walks in the model object after training
         w2vparams : dict
             dictionary of parameters to pass to gensim's word2vec
-            of relevance is "size" (length of resulting embedding vector)
+            Don't set the embedding dimensions through arguments here.
         """
         if type(threads) is not int:
             raise ValueError("Threads argument must be an int!")
@@ -151,13 +177,13 @@ class Node2Vec(BaseNodeEmbedder):
 
     def fit(self, nxGraph, verbose=1):
         """
-        NOTE: Currently only support str as node name for graph
+        NOTE: Currently only support str or int as node name for graph
         Parameters
         ----------
-        nxGraph : NetworkX.Graph
+        nxGraph : graph data
             Graph to embed
-            Can be any graph type that's supported by CSRGraphs
-            (NetworkX, numpy 2d array, scipy CSR matrix)
+            Can be any graph type that's supported by CSRGraph library
+            (NetworkX, numpy 2d array, scipy CSR matrix, CSR matrix components)
         verbose : bool
             Whether to print output while working
         """
@@ -208,6 +234,8 @@ class Node2Vec(BaseNodeEmbedder):
     def predict(self, node_name):
         """
         Return vector associated with node
+        node_name : str or int
+            either the node ID or node name depending on graph format
         """
         # current hack to work around word2vec problem
         # ints need to be str -_-
@@ -234,14 +262,36 @@ class SKLearnEmbedder(BaseNodeEmbedder):
             normalize_graph=True,
             **kwargs):
         ""
-
+        Creates a node embedder through a SKLearn API model
+        The SKLearn model must implement the fit_transform method
+            (eg. Isomap, UMAP, PCA, etc.)
+            
+        Model parameters are also passed directly into this constructor
+        
+        embedder : SKLearn model implementing fit_transform
+            This is not a model instance, rather the class name object
+            
+        normalize_graph : bool
+            Whether to normalize the graph edge weights before embedding
+            This calls the optimized CSRGraph routine
+            
+        **kwargs : SKLearn model keyword arguments
+            These arguments are passed directly to the construction of the model
         ""
         self.embedder = embedder(**kwargs)
         self.normalize_graph = normalize_graph
 
     def fit(self, graph, verbose=1):
         """
-
+        NOTE: Currently only support str or int as node name for graph
+        Parameters
+        ----------
+        nxGraph : graph data
+            Graph to embed
+            Can be any graph type that's supported by CSRGraph library
+            (NetworkX, numpy 2d array, scipy CSR matrix, CSR matrix components)
+        verbose : bool
+            Whether to print output while working
         """
         G = CSRGraph(graph)
         if self.normalize_graph:
@@ -253,6 +303,8 @@ class SKLearnEmbedder(BaseNodeEmbedder):
 
     def predict(self, node_name):
         """
-
+        Return vector associated with node
+        node_name : str or int
+            either the node ID or node name depending on graph format
         """
         return self.model[node_name]
