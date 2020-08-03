daf0042e74d514c5e06572325b1907b4cc423c85
Matt Ranger <20938940+VHRanger@users.noreply.github.com>
2020-08-02 22:52:35 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nodevectors",
-    version="0.1.20",
+    version="0.1.22",
     license='MIT',
     description='Fast network node embeddings',
     author='Matt Ranger',
