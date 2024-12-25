df12d7c694d202af4b7098af229537ee13e925eb
20938940+VHRanger@users.noreply.github.com <matthieu.ranger@outlook.com>
2024-12-25 12:23:01 -0500

    updated to use uv

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
+import os
+from pathlib import Path
 from setuptools import setup, find_packages
+import tomllib
 
+PROJECT_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
+
+with open(PROJECT_DIR / "pyproject.toml", "rb") as f:
+    tm = tomllib.load(f)['project']
+
+print(tm)
+    
 setup(
-    name="nodevectors",
-    version="0.1.23",
-    license='MIT',
-    description='Fast network node embeddings',
-    author='Matt Ranger',
-    url='https://github.com/VHRanger/nodevectors/',
+    name=tm['name'],
+    version=tm['version'],
+    license=tm['license'],
+    author=tm['authors'][0]['name'],
+    url=tm['urls']['github'],
     packages=find_packages(),
-    keywords=['graph', 'network', 'embedding', 'node2vec'],
     package_data={
         # If any package contains *.txt or *.rst files, include them:
         '': ['*.md', '*.txt', '*.rst']
     },
-    install_requires=[
-    'csrgraph',
-    'gensim',
-    'networkx <= 3.0',
-    'numba',
-    'numpy',
-    'pandas >= 1.0',
-    'scipy',
-    'scikit-learn',
-    ],
+    install_requires=tm['dependencies'],
 )
